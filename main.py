54b4f66f91d4583b1072df218551fd0155cf41d6
JAYSURIYA02 <jaysuriya.rv@gmail.com>
2025-09-14 14:29:50 +0530

    comp

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 import json
 from deepface import DeepFace
-
-
-
-result = DeepFace.verify(img1_path=r"persons\JAY2.JPG",img2_path=r"persons\JAY3.JPG")
+result = DeepFace.verify(img1_path=r"persons\notjay1.jpg",img2_path=r"persons\JAY2.jpg")
 result["verified"] = bool(result["verified"])
 print(json.dumps(result, indent=2))
 
+result2 = DeepFace.find(img_path=r"JAY3.jpg", db_path=r"persons")
+print(result2)
 
-
+result_analysis = DeepFace.analyze(img_path = r"persons\notjay3.jpg")
+print(json.dumps(result_analysis))
